7584833d659473f1ef7542e706c5deedbf6a8cc3
Wouter Beek <me@wouterbeek.com>
2013-03-28 11:58:13 +0100

    Added server communication commands to the readme.

--- theapp/gen/com/example/funwithtriples/R.java
+++ theapp/gen/com/example/funwithtriples/R.java
@@ -21,7 +21,6 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-<<<<<<< HEAD
         public static final int funwithtriples=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
@@ -30,19 +29,6 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-<<<<<<< HEAD
-        public static final int axiomlist=0x7f030001;
-=======
-        public static final int ic_launcher=0x7f020000;
-    }
-    public static final class id {
-        public static final int action_settings=0x7f080000;
-    }
-    public static final class layout {
-        public static final int activity_main=0x7f030000;
->>>>>>> Initial test commit
-=======
->>>>>>> Auto-generated
     }
     public static final class menu {
         public static final int main=0x7f070000;
